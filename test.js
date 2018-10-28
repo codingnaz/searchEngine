8cae2a906807061d044d5f94113729abb8327f78
Nazmul <saimon_01@yahoo.com>
2018-10-28 17:48:12 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,16 +3,16 @@ var app = express();
 var requests = require('request');
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, () =>{
   //this is just an indicator that the server is properly functioning. 
   console.log('Server listening to requests');
 });
 //will render a basic search menu 
-app.get('/search',function(req,res){
+app.get('/search',(req,res) => {
   res.render('search');
 });
 //once the search is complete you will be redirected to the results page.
-app.get("/results", function(req,res){
+app.get("/results", (req,res) => {
 var query= req.query.search;
 var url = "//http://www.omdbapi.com/?i=tt3896198&apikey=thewdb" + query;
   requests(url,function(err,response,body));
@@ -21,7 +21,7 @@ var url = "//http://www.omdbapi.com/?i=tt3896198&apikey=thewdb" + query;
       res.render("results", {data:data});
     }
   
-  app.get("*", function(req,res){
+  app.get("*", (req,res) => {
     res.send("404 error,page could not be found!")
   });
 });
